e637d88951cd8ced1a813b14c923453e4303be15
Muhammad Ali <140586167+Dev-Muhammad-Ali@users.noreply.github.com>
2024-10-08 17:04:47 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,63 @@
+let Hours = document.querySelector("#hours");
+let minutes = document.querySelector("#minutes");
+let Seconds = document.querySelector("#seconds");
+let zone = document.querySelector("#zone");
+
+function TIME(){
+    let date = new Date();
+    let h = date.getHours();
+    let m = date.getMinutes();
+    let s = date.getSeconds(); 
+
+    if(h>=12){
+        zone.innerHTML = "PM"
+    }
+    else{
+        zone.innerHTML = "AM"
+    }
+
+    if(h == 0){
+        h = 24;
+    }
+
+    if(h>12){
+        h = h - 12;
+        if(h<10){
+            Hours.innerHTML = "0" + h;
+        }
+        else{
+            Hours.innerHTML = h;
+        }
+        
+    }
+    else{
+        if(h<10){
+            Hours.innerHTML = "0" + h;
+        }
+        else{
+            Hours.innerHTML = h;
+        }
+    }
+    
+    if(m<10){
+        m = "0" + m;
+        minutes.innerHTML = m;
+    }
+    else{
+        minutes.innerHTML = m;
+    }
+    
+    if(s<10){
+        s = "0" + s;
+        Seconds.innerHTML = s;
+    }
+    else{
+        Seconds.innerHTML = s;
+    }
+}
+
+function startClock(){
+    TIME();
+    setInterval(TIME,1000);
+}
+startClock();
